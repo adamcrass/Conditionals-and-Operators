ab78bbf0bff45d2e5203b96d82c142e2b3add631
AJ <37306837+adamcrass@users.noreply.github.com>
2025-03-12 14:20:43 -0700

    Add files via upload

---
+++ Conditionals and Operators/script.js
@@ -0,0 +1,16 @@
+var num1 = prompt("What's your number?");
+
+if (num1 > 10) {
+  console.log("Your number is bigger than 10!");
+} else if (num1 < 10) {
+  console.log("Your number is smaller than 10!");
+} else {
+  console.log("Your number is exactly 10!");
+}
+
+var num2 = prompt("What's your 2nd number?");
+
+if ((num2 = !10));
+{
+  console.log("Your number is not 10!");
+}
